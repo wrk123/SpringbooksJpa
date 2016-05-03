4d3473edf038e0905fb1f27db557623e4482245a
harshit-pc <harshit-pc@harshit>
2016-05-03 15:01:57 +0530

    Changes made to DAO classes

--- SpringbooksJpa/src/main/java/com/dao/BooksDAO.java
+++ SpringbooksJpa/src/main/java/com/dao/BooksDAO.java
@@ -1,9 +1,11 @@
 package com.dao;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.repository.CrudRepository;
 import com.model.Books;
 
-
+@Transactional
 public interface BooksDAO extends CrudRepository<Books, Long>{
 
 }
--- SpringbooksJpa/src/main/java/com/dao/UserDAO.java
+++ SpringbooksJpa/src/main/java/com/dao/UserDAO.java
@@ -1,10 +1,14 @@
 package com.dao;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.model.User;
 
+@Transactional
 public interface UserDAO extends CrudRepository<User, Long>{
 
+	public User findByEmail(String email);
 	
 }
